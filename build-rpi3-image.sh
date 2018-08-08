22bde2689925c113a711f55e5345ae0021576aa4
anemonium <42048221+anemonium@users.noreply.github.com>
2018-08-08 23:02:53 +0200

    Delete build-rpi3-image.sh

--- build-rpi3-image.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -xe
-
-cp Dockerfile Dockerfile.armv7
-sed -i "s|FROM .*|FROM resin/rpi-raspbian:jessie|g" Dockerfile.armv7
-sed -i 's|ENV SEAFILE_SERVER_URL.*|ENV SEAFILE_SERVER_URL https://github.com/haiwen/seafile-rpi/releases/download/v${SEAFILE_SERVER_VERSION}/seafile-server_${SEAFILE_SERVER_VERSION}_stable_pi.tar.gz|g' Dockerfile.armv7
-docker build . -f Dockerfile.armv7 -t anemonium/seafile-server:raspberrypi3-latest
