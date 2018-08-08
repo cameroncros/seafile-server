446a3775ac24f5f1630b29e53c4c53eea5ba69f5
anemonium <42048221+anemonium@users.noreply.github.com>
2018-08-08 21:39:24 +0200

    Update build-rpi3-image.sh

--- build-rpi3-image.sh
+++ build-rpi3-image.sh
@@ -2,15 +2,7 @@
 
 set -xe
 
-if [ ! -z $1 ]; then
-  TAG=$1
-else
-  TAG=fjctp/seafile-server:raspberrypi3-latest
-fi
-
-echo "Using tag name: $TAG"
-
 cp Dockerfile Dockerfile.armv7
 sed -i "s|FROM .*|FROM resin/rpi-raspbian:jessie|g" Dockerfile.armv7
-sed -i 's|ENV SEAFILE_SERVER_URL.*|ENV SEAFILE_SERVER_URL https://github.com/haiwen/seafile-rpi/releases/download/v6.3.2/seafile-server_6.3.2_stable_pi.tar.gz|g' Dockerfile.armv7
-docker build . -f Dockerfile.armv7 -t $TAG
+sed -i 's|ENV SEAFILE_SERVER_URL.*|ENV SEAFILE_SERVER_URL https://github.com/haiwen/seafile-rpi/releases/download/v${SEAFILE_SERVER_VERSION}/seafile-server_${SEAFILE_SERVER_VERSION}_stable_pi.tar.gz|g' Dockerfile.armv7
+docker build . -f Dockerfile.armv7 -t anemonium/seafile-server:raspberrypi3-latest
