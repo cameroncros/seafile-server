70c5a7969c5d7c23a4c54890368effc26c0cfc96
anemonium <42048221+anemonium@users.noreply.github.com>
2019-01-08 23:05:34 +0100

    Update seafile.sh

--- seafile/seafile.sh
+++ seafile/seafile.sh
@@ -15,8 +15,8 @@ echo ""
 SCRIPT=$(readlink -f "$0")
 INSTALLPATH=$(dirname "${SCRIPT}")
 TOPDIR=$(dirname "${INSTALLPATH}")
-default_ccnet_conf_dir=${SHAREDDIR}/ccnet
-central_config_dir=${SHAREDDIR}/conf
+default_ccnet_conf_dir=${TOPDIR}/shared/ccnet
+central_config_dir=${TOPDIR}/shared/conf
 seaf_controller="${INSTALLPATH}/seafile/bin/seafile-controller"
 
 
